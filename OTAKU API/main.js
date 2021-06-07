e7a597cffcd28da23879c82a0e4e358502e7e472
Sofia Lopez <anasolori.92@gmail.com>
2021-06-07 11:19:42 -0500

    AGREGAR BOTON API OTAKU

--- OTAKU API/main.js
+++ OTAKU API/main.js
@@ -1,3 +1,10 @@
-fetch('https://animechan.vercel.app/api/random')
-  .then((response) => response.json())
-  .then((quote) => console.log(quote));
+function animeQuote() {
+  fetch('https://animechan.vercel.app/api/random')
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById('anime').innerHTML = 'Anime: ' + data.anime;
+      document.getElementById('character').innerHTML =
+        'Personaje: ' + data.character;
+      document.getElementById('quote').innerHTML = 'Cita: ' + data.quote;
+    });
+}
